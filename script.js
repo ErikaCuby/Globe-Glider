eee44ea5273efedf99f6af99e459012db869c27f
ErikaCuby <159401677+ErikaCuby@users.noreply.github.com>
2024-02-23 05:48:02 +0100

    Injected Bergen and New York real time from js

--- script.js
+++ script.js
@@ -19,6 +19,28 @@ function updateTime() {
       "h:mm:ss [<span>]A[</span>]"
     );
   }
+  //Bergen
+  let bergenElement = document.querySelector("#bergen");
+  if (singaporeElement) {
+    let bergenDateElement = bergenElement.querySelector(".date");
+    let bergenTimeElement = bergenElement.querySelector(".time");
+    let bergenTime = moment().tz("Europe/Oslo");
+    bergenDateElement.innerHTML = bergenTime.format("MMMM Do");
+    bergenTimeElement.innerHTML = bergenTime.format(
+      "h:mm:ss [<span>]A[</span>]"
+    );
+  }
+  //New York
+  let newYorkElement = document.querySelector("#new-york");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss [<span>]A[</span>]"
+    );
+  }
 }
 updateTime();
 setInterval(updateTime, 1000);
